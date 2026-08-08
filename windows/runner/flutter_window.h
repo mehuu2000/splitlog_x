eb2e68c99ab0912e35c7bf799d04d8874a7ebfa8
mehuu2000 <mehuu2000@gmail.com>
2026-08-08 23:17:01 +0900

    微修正

--- windows/runner/flutter_window.h
+++ windows/runner/flutter_window.h
@@ -30,7 +30,7 @@ class FlutterWindow : public Win32Window {
   // Win32Window:
   bool OnCreate() override;
   void OnDestroy() override;
- LRESULT MessageHandler(HWND window, UINT const message, WPARAM const wparam,
+  LRESULT MessageHandler(HWND window, UINT const message, WPARAM const wparam,
                          LPARAM const lparam) noexcept override;
 
  private:
